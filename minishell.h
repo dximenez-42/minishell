2ab2c3982812ad5d39edf9af1bf9a3a8b9bf909b
blasdo <72580092+blasdo@users.noreply.github.com>
2024-04-19 13:31:12 +0200

    modified struct proposal

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: bvelasco <bvelasco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 14:01:24 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/04/18 16:16:46 by bvelasco         ###   ########.fr       */
+/*   Updated: 2024/04/19 13:30:54 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,21 @@ typedef struct s_commmand
 	int		fds[3];
 }				t_command;
 
+/*
+ * NOC is the number of commands (command + arguments)
+ * cmds are the commands itself (array)
+ * env is the complete environment ({"name", "value"}, {"name", "value"})
+ * info is the additional info:
+ * 	-If >=0 is information or warnings, see the information list
+ * 	-if <0 is an error. 
+ */
 typedef struct s_input
 {
 	__uint8_t	noc;
 	t_command	*cmds;
-	char		**env;
-	__uint8_t	errc;
+	char		***env;
+	__uint8_t	info;
 }				t_input;
 /*End of common structs*/
+
 #endif
