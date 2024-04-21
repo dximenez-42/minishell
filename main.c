064b7097041b67193bb2997ff4afe35486971a73
Dani X <danixidev@gmail.com>
2024-04-21 12:54:20 +0200

    Init input

--- main.c
+++ main.c
@@ -6,11 +6,28 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 17:49:21 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/04/20 14:45:58 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/04/21 12:51:29 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	init_input(t_input *input)
+{
+	input = malloc(sizeof(t_input));
+	input->noc = 1;
+	input->cmds = malloc(1 * sizeof(t_command));
+	input->cmds->info = 0;
+	input->cmds->args = malloc(2 * sizeof(char *));
+	input->cmds->args[0] = malloc(3 * sizeof(char));
+	input->cmds->args[0] = "cd";
+	input->cmds->args[1] = malloc(6 * sizeof(char));
+	input->cmds->args[1] = "libft";
+	input->cmds->fds[FDIN] = FDIN;
+	input->cmds->fds[FDOUT] = FDOUT;
+	input->cmds->fds[FDERROR] = FDERROR;
+}
+
 
 int	main(int argc, char *argv[], char *envp[])
 {
@@ -27,7 +44,11 @@ int	main(int argc, char *argv[], char *envp[])
 		{
 			add_history(line);
 			// parse();
-			// exec();
+			init_input(input);
+			if (input->noc == 1)
+				exec_one(&input->cmds[0], input->env);
+			else
+				exec_multiple(&input->cmds, input->env);
 		}
 	}
 	return (0);
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 14:01:24 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/04/19 22:41:20 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/04/21 12:32:45 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,9 @@ enum
 	FDERROR = 2
 };
 
+
+void	exec_one(t_command *cmd, t_list *env);
+void	exec_multiple(t_command **cmd, t_list *env);
 
 void	cd_builtin(t_command *cmd, t_list *env);
 
