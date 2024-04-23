15d0a33d618ba91a7dff26c4b9dcf59c744723a1
blasdo <72580092+blasdo@users.noreply.github.com>
2024-04-23 18:14:16 +0000

    modified main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 18:29:48 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/04/23 15:49:07 by bvelasco         ###   ########.fr       */
+/*   Updated: 2024/04/23 18:13:52 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main(int argc, char *argv[], char *envp[])
 	char 	**envc;
 
 	env = parse_env(envp);
-	set_env_var(&env, argv[1], ft_strdup(argv[2]));
+	set_env_var(&env, argv[1], argv[2]);
 	envc = ft_getenv(env);
 	for (int i = 0; envc[i]; i++)
 		printf("%s\n", envc[i]);
