ee74cf2bb33e8a915a8ec74a30f97cb616debe32
Borja Velasco <72580092+blasdo@users.noreply.github.com>
2024-05-05 19:59:23 +0200

    get_quotelen now works with double quotes

--- src/parser/parse_phases/quotes.c
+++ src/parser/parse_phases/quotes.c
@@ -6,29 +6,35 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 18:19:08 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/05/05 15:16:29 by bvelasco         ###   ########.fr       */
+/*   Updated: 2024/05/05 19:57:34 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int get_quotelen(char *quote)
+/*
+	get total len of quotes (not expanded)
+*/
+int	get_quotelen(char *quote, t_list *varlist)
 {
-	int	clen;
+	int	i;
 
-	clen = 0;
-	while (quote[++clen] && quote[clen] != '\'');
-	if (quote[clen] == '\'')
-		clen++;
-	return (clen);
+	i = 0;
+	if (*quote == '"')
+		while (quote[++i] && quote[i] != '"');
+	else if (*quote == '\'')
+		while (quote[++i] && quote[i] != '\'');
+	if (quote[i] == '\'' || quote[i] == '\"')
+		i++;
+	return (i);
 }
-void copy_simple_quoute(char *rstr, char *str, int *rc, int *pc)
+void	copy_simple_quoute(char *rstr, char *str, int *rc, int *pc)
 {
 	int	limit;
 	int	i;
 
 	i = 0;
-	limit = get_quotelen(rstr + *rc);
+	limit = get_quotelen(rstr + *rc, NULL);
 	while (i <= limit)
 	{
 		*(str + *pc + i) = *(rstr + *rc + i);
@@ -38,3 +44,10 @@ void copy_simple_quoute(char *rstr, char *str, int *rc, int *pc)
 	*pc += i;
 }
 
+void	proc_quotes(char *rstr, char *str, int *counters[])
+{
+	int	i;
+	int	j;
+
+	if (rstr[j]);
+}
