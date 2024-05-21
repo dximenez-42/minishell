4ccd182e9e82de0165fff364ab631b0b9d2c73c5
blasdo <72580092+blasdo@users.noreply.github.com>
2024-05-21 15:22:29 +0200

    nominete ok in tokenization.c

--- src/parser/parse_phases/tokenization.c
+++ src/parser/parse_phases/tokenization.c
@@ -6,7 +6,7 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 16:53:06 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/05/14 18:10:22by bvelasco         ###   ########.fr       */
+/*   Updated: 2024/05/21 15:21:45 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static char	*proc_arg_token(char *first_char, int *pos)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (first_char[i] &&!ft_isspace(first_char[i]))
@@ -22,12 +22,12 @@ static char	*proc_arg_token(char *first_char, int *pos)
 		if (ft_isquote(first_char[i]))
 		{
 			i += get_quotelen(first_char + i);
-			continue;
+			continue ;
 		}
 		if (first_char[i] == '$')
 		{
 			i += get_varname_len(first_char + ++i);
-			continue ;	
+			continue ;
 		}
 		if (ft_isredir(first_char[i]))
 			break ;
@@ -37,7 +37,7 @@ static char	*proc_arg_token(char *first_char, int *pos)
 	return (ft_substr(first_char, 0, i));
 }
 
-static char *proc_redir_token(char *first_char, int *pos)
+static char	*proc_redir_token(char *first_char, int *pos)
 {
 	int	i;
 
@@ -53,22 +53,20 @@ static char *proc_redir_token(char *first_char, int *pos)
 		if (first_char[i] == '<' || first_char[i] == '>')
 		{
 			*pos += i;
-			return(ft_substr(first_char, 0, i));
+			return (ft_substr(first_char, 0, i));
 		}
 		i++;
 	}
 	*pos += i;
 	return (ft_substr(first_char, 0, i));
-
 }
-
 
 static int	process_token(t_list **tok_list, char *first_char, int *pos)
 {
 	int		i;
 	char	*buffer;
 	t_list	*node;
-	t_token *tok;
+	t_token	*tok;
 
 	i = 0;
 	tok = malloc(sizeof(t_token));
@@ -85,19 +83,19 @@ static int	process_token(t_list **tok_list, char *first_char, int *pos)
 	if (!buffer)
 		return (free(tok), error(ERRMEM), 1);
 	tok->value = buffer;
-	ft_lstadd_back(tok_list, ft_lstnew_type(OTHER, (t_content) (void *) tok));
+	ft_lstadd_back(tok_list, ft_lstnew_type(OTHER, (t_content)(void *) tok));
 	return (0);
 }
 
-t_content	tokenize_command(t_content content,  t_type type)
-{ 
+t_content	tokenize_command(t_content content, t_type type)
+{
 	int		i;
 	t_list	*tok_list;
 	t_list	*node;
 	t_token	*token;
 	char	*raw_cmd;
 
-	i = 0;	
+	i = 0;
 	tok_list = NULL;
 	raw_cmd = content.str;
 	while (raw_cmd[i])
@@ -110,5 +108,5 @@ t_content	tokenize_command(t_content content,  t_type type)
 		}
 		i++;
 	}
-	return ((t_content) (void *)tok_list);
+	return ((t_content)(void *)tok_list);
 }
