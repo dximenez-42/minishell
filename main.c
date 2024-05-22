44da4abed943fde0bf04ce7b1c7a2a4ee5183980
Dani X <danixidev@gmail.com>
2024-05-22 23:09:23 +0200

    New test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 17:49:21 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/05/21 18:58:43 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/05/22 23:07:19 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,34 @@ void	init_input(t_input **input, t_list *env, char *line)
 	(*input)->cmds[3] = NULL;
 }
 
+// void	init_input(t_input **input, t_list *env, char *line)
+// {
+// 	(*input) = malloc(sizeof(t_input));
+// 	(*input)->noc = 2;
+// 	(*input)->env = env;
+// 	(*input)->cmds = malloc(3 * sizeof(t_command *));
+
+// 	(*input)->cmds[0] = malloc(sizeof(t_command));
+// 	(*input)->cmds[0]->info = 1;
+// 	(*input)->cmds[0]->args = malloc(3 * sizeof(char *));
+// 	(*input)->cmds[0]->args[0] = ft_strdup("ls");
+// 	(*input)->cmds[0]->args[1] = NULL;
+// 	(*input)->cmds[0]->fds[FDIN] = STDIN_FILENO;
+// 	(*input)->cmds[0]->fds[FDOUT] = STDOUT_FILENO;
+// 	(*input)->cmds[0]->fds[FDERROR] = STDERR_FILENO;
+
+// 	(*input)->cmds[1] = malloc(sizeof(t_command));
+// 	(*input)->cmds[1]->info = 1;
+// 	(*input)->cmds[1]->args = malloc(3 * sizeof(char *));
+// 	(*input)->cmds[1]->args[0] = ft_strdup("wc");
+// 	(*input)->cmds[1]->args[1] = ft_strdup("-l");
+// 	(*input)->cmds[1]->args[2] = NULL;
+// 	(*input)->cmds[1]->fds[FDIN] = STDIN_FILENO;
+// 	(*input)->cmds[1]->fds[FDOUT] = STDOUT_FILENO;
+// 	(*input)->cmds[1]->fds[FDERROR] = STDERR_FILENO;
+	
+// 	(*input)->cmds[2] = NULL;
+// }
 
 int	main(int argc, char *argv[], char *envp[])
 {
