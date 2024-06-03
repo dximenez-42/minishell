2ea1dac3b0eebc5b2623c2a3068221bff9ad07de
Borja Velasco <72580092+blasdo@users.noreply.github.com>
2024-06-03 19:43:21 +0200

    fix leaks heredoc

--- src/parser/parse_phases/heredoc.c
+++ src/parser/parse_phases/heredoc.c
@@ -6,11 +6,12 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 15:50:22 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/06/03 15:54:39 by bvelasco         ###   ########.fr       */
+/*   Updated: 2024/06/03 19:35:03 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
 int	creat_ext_heredoc(t_list *env, char *delimitor)
 {
 	int		fds[2];
@@ -61,7 +62,7 @@ static char	*remove_quotes(char *str)
 	int		i;
 	int		j;
 	int		len_without_quotes;
-	
+
 	i = 0;
 	len_without_quotes = 0;
 	while (str[i])
@@ -71,6 +72,8 @@ static char	*remove_quotes(char *str)
 	}
 	i = 0;
 	result = malloc(len_without_quotes + 1);
+	if (!result)
+		return (0);
 	j = 0;
 	while (str[i])
 	{
@@ -78,8 +81,10 @@ static char	*remove_quotes(char *str)
 			result[j++] = str[i];
 		i++;
 	}
+	result[j] = 0;
 	return (result);
 }
+
 static __uint8_t	has_quotes(char	*str)
 {
 	while (*str)
@@ -94,6 +99,7 @@ static __uint8_t	has_quotes(char	*str)
 int	creat_heredoc(t_list *env, char *delim)
 {
 	int	fd;
+
 	if (has_quotes(delim))
 		fd = creat_not_ext_heredoc(remove_quotes(delim));
 	else
